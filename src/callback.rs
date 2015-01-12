19d97734b565f4f8112d5a1e5f1535f47faf31d6
Minjie Zha <minjiezha@gmail.com>
2015-01-11 20:40:04 -0800

    adjust format in callback

--- src/callback.rs
+++ src/callback.rs
@@ -10,42 +10,42 @@ pub type CallbackResult = Result<CallbackDecision, String>;
 
 pub trait HttpParserCallback {
     #[allow(unused_variables)]
-    fn on_message_begin(&mut self, parser : &mut HttpParser) -> CallbackResult {
+    fn on_message_begin(&mut self, parser: &mut HttpParser) -> CallbackResult {
         Ok(CallbackDecision::Nothing)
     }
 
     #[allow(unused_variables)]
-    fn on_url(&mut self, parser : &mut HttpParser, data : &[u8],) -> CallbackResult {
+    fn on_url(&mut self, parser: &mut HttpParser, data: &[u8],) -> CallbackResult {
         Ok(CallbackDecision::Nothing)
     }
 
     #[allow(unused_variables)]
-    fn on_status(&mut self, parser : &mut HttpParser, data : &[u8]) -> CallbackResult {
+    fn on_status(&mut self, parser: &mut HttpParser, data: &[u8]) -> CallbackResult {
         Ok(CallbackDecision::Nothing)
     }
 
     #[allow(unused_variables)]
-    fn on_header_field(&mut self, parser : &mut HttpParser, data : &[u8]) -> CallbackResult {
+    fn on_header_field(&mut self, parser: &mut HttpParser, data: &[u8]) -> CallbackResult {
         Ok(CallbackDecision::Nothing)
     }
 
     #[allow(unused_variables)]
-    fn on_header_value(&mut self, parser : &mut HttpParser, data : &[u8]) -> CallbackResult {
+    fn on_header_value(&mut self, parser: &mut HttpParser, data: &[u8]) -> CallbackResult {
         Ok(CallbackDecision::Nothing)
     }
 
     #[allow(unused_variables)]
-    fn on_headers_complete(&mut self, parser : &mut HttpParser) -> CallbackResult {
+    fn on_headers_complete(&mut self, parser: &mut HttpParser) -> CallbackResult {
         Ok(CallbackDecision::Nothing)
     }
 
     #[allow(unused_variables)]
-    fn on_body(&mut self, parser : &mut HttpParser, data : &[u8]) -> CallbackResult {
+    fn on_body(&mut self, parser: &mut HttpParser, data: &[u8]) -> CallbackResult {
         Ok(CallbackDecision::Nothing)
     }
 
     #[allow(unused_variables)]
-    fn on_message_complete(&mut self, parser : &mut HttpParser) -> CallbackResult {
+    fn on_message_complete(&mut self, parser: &mut HttpParser) -> CallbackResult {
         Ok(CallbackDecision::Nothing)
     }
 }
