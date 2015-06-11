318c3d783beaa352a2a3070349505810905fe32a
Minjie Zha <minjiezha@gmail.com>
2015-06-11 00:13:00 -0700

    refactor the normal url char validation

--- src/parser.rs
+++ src/parser.rs
@@ -85,41 +85,6 @@ const CHUNKED : &'static str = "chunked";
 const KEEP_ALIVE : &'static str = "keep-alive";
 const CLOSE : &'static str = "close";
 
-static NORMAL_URL_CHAR : &'static [u8; 32] = &[
-    //   0 nul    1 soh    2 stx    3 etx    4 eot    5 enq    6 ack    7 bel   
-            0    |   0    |   0    |   0    |   0    |   0    |   0    |   0,       
-    //   8 bs     9 ht    10 nl    11 vt    12 np    13 cr    14 so    15 si
-            0    |   2    |   0    |   0    |   16   |   0    |   0    |   0, // TODO add T()
-    //  16 dle   17 dc1   18 dc2   19 dc3   20 dc4   21 nak   22 syn   23 etb
-            0    |   0    |   0    |   0    |   0    |   0    |   0    |   0,       
-    //  24 can   25 em    26 sub   27 esc   28 fs    29 gs    30 rs    31 us    
-            0    |   0    |   0    |   0    |   0    |   0    |   0    |   0,       
-    //  32 sp    33  !    34  "    35  #    36  $    37  %    38  &    39  '    
-            0    |   2    |   4    |   0    |   16   |   32   |   64   |  128,      
-    //  40  (    41  )    42  *    43  +    44  ,    45  -    46  .    47  /    
-            1    |   2    |   4    |   8    |   16   |   32   |   64   |  128,      
-    //  48  0    49  1    50  2    51  3    52  4    53  5    54  6    55  7    
-            1    |   2    |   4    |   8    |   16   |   32   |   64   |  128,      
-    //  56  8    57  9    58  :    59  ;    60  <    61  =    62  >    63  ?    
-            1    |   2    |   4    |   8    |   16   |   32   |   64   |   0,       
-    //  64  @    65  A    66  B    67  C    68  D    69  E    70  F    71  G    
-            1    |   2    |   4    |   8    |   16   |   32   |   64   |  128,      
-    //  72  H    73  I    74  J    75  K    76  L    77  M    78  N    79  O    
-            1    |   2    |   4    |   8    |   16   |   32   |   64   |  128,      
-    //  80  P    81  Q    82  R    83  S    84  T    85  U    86  V    87  W    
-            1    |   2    |   4    |   8    |   16   |   32   |   64   |  128,      
-    //  88  X    89  Y    90  Z    91  [    92  \    93  ]    94  ^    95  _    
-            1    |   2    |   4    |   8    |   16   |   32   |   64   |  128,      
-    //  96  `    97  a    98  b    99  c   100  d   101  e   102  f   103  g    
-            1    |   2    |   4    |   8    |   16   |   32   |   64   |  128,      
-    // 104  h   105  i   106  j   107  k   108  l   109  m   110  n   111  o   
-            1    |   2    |   4    |   8    |   16   |   32   |   64   |  128,      
-    // 112  p   113  q   114  r   115  s   116  t   117  u   118  v   119  w   
-            1    |   2    |   4    |   8    |   16   |   32   |   64   |  128,      
-    // 120  x   121  y   122  z   123  {   124  |   125  }   126  ~   127 del
-            1    |   2    |   4    |   8    |   16   |   32   |   64   |   0,
-            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
-
 static UNHEX : &'static [i8; 256] = &[
     -1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,
     -1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,
@@ -153,9 +118,13 @@ fn is_header_char(strict: bool, ch: u8) -> bool {
     }
 }
 
-fn is_url_char(hp : &HttpParser, ch : u8) -> bool {
-    let res = (NORMAL_URL_CHAR[(ch >> 3) as usize] & (1 << ((ch & 7) as usize))) != 0;
-    res || (!hp.strict && (ch & 0x80) > 0)
+fn is_normal_url_char(ch: u8) -> bool {
+    // refer to http_parser.c or ascii table for characters
+    ch == b'!' || ch == b'"' || (ch >= b'$' && ch <= b'>') || (ch >= b'@' && ch <= b'~')
+}
+
+fn is_url_char(strict: bool, ch: u8) -> bool {
+    is_normal_url_char(ch) || (!strict && ((ch & 0x80) > 0 || ch == 9 || ch == 12))
 }
 
 fn lower(ch : u8) -> u8 {
@@ -1445,7 +1414,7 @@ impl HttpParser {
                 }
             },
             State::ReqPath => {
-                if is_url_char(self, ch) {
+                if is_url_char(self.strict, ch) {
                     return s;
                 }
 
@@ -1456,7 +1425,7 @@ impl HttpParser {
                 }
             },
             State::ReqQueryStringStart | State::ReqQueryString => {
-                if is_url_char(self, ch) {
+                if is_url_char(self.strict, ch) {
                     return State::ReqQueryString;
                 }
 
@@ -1467,7 +1436,7 @@ impl HttpParser {
                 }
             },
             State::ReqFragmentStart => {
-                if is_url_char(self, ch) {
+                if is_url_char(self.strict, ch) {
                     return State::ReqFragment;
                 }
 
@@ -1478,7 +1447,7 @@ impl HttpParser {
                 }
             },
             State::ReqFragment => {
-                if is_url_char(self, ch) {
+                if is_url_char(self.strict, ch) {
                     return s;
                 }
 
